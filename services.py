e91754bd7e533397e8e15f0d698dcd1631a7091b
Sam Jarvis <jarvisam@gmail.com>
2018-08-04 00:29:58 +0100

    refactor into packages, allow dual faced cards, prioritise full name match, prioritise sub-word match

---
+++ services.py
@@ -0,0 +1,73 @@
+import os
+import json
+import utils
+from PIL import Image
+from io import BytesIO
+from requests import get
+
+import utils
+
+def scryfall(name):
+    try:
+        scryfall = json.loads(get("https://api.scryfall.com/cards/search?q={}".format(name)).text)
+        # get the normal sized image, strip query param
+        if (scryfall['data'][0].get('card_faces')):
+            card_face_urls = [card_face['image_uris']['normal'].split('?')[0] for card_face in scryfall['data'][0]['card_faces']]
+            return ({
+                'imageurls': card_face_urls,
+                'name': scryfall['data'][0]['name'],
+                'dualfaced': True
+            })
+
+
+        else:
+            return ({
+                'imageurls': [scryfall['data'][0]['image_uris']['normal'].split("?")[0]],
+                'name': scryfall['data'][0]['name'],
+                'dualfaced': False
+            })
+    except Exception as e:
+        print("scryfall api call error")
+        print(e)
+        return False
+
+def mtgJson(name):
+    try:
+        apinames = json.loads(get("http://mtgapi.samus.nl/cardjson/{}".format(name)).text)
+        return apinames
+    except:
+        print("samus.nl api call error")
+        return False
+
+def cardFetch(name):
+    corseMatch = mtgJson(name)
+    closeMatch = utils.properNames(corseMatch, name)
+    fineMatch = utils.sequenceMatch(closeMatch, name)
+    print(corseMatch)
+    print(closeMatch)
+    print(fineMatch)
+    print("Entered name:{}, Best Match:{}".format(name, fineMatch))
+    return scryfall(fineMatch)
+
+def buildDualFaced(cardData):
+    cachedImages = os.listdir('cache')
+    sluggedFileName = '{}.jpg'.format(utils.slugify(cardData['name']))
+    if sluggedFileName in cachedImages:
+        print("file in cache")
+        return sluggedFileName
+    else:
+        print("file not in cache")
+        imageRequests = map(get,cardData['imageurls'])
+        images = [Image.open(img) for img in [BytesIO(image.content) for image in imageRequests]]
+        widths, heights = zip(*(i.size for i in images))
+        total_width = 976
+        max_height = 680
+        new_im = Image.new('RGB', (total_width, max_height))
+
+        x_offset = 0
+
+        for im in images:
+          new_im.paste(im, (x_offset,0))
+          x_offset += im.size[0]
+        new_im.save('./cache/{}'.format(sluggedFileName))
+        return sluggedFileName
---
+++ utils.py
@@ -0,0 +1,127 @@
+import re
+from difflib import SequenceMatcher
+
+def slugify(name):
+    return "".join(x for x in name if x.isalnum())
+
+
+class LukesCards():
+    def __init__(self):
+        self.ITERATOR = -1
+        self.cards = [
+            'Birthing Pod',
+            'Gitaxian Probe',
+            'Stoneforge Mystic',
+            'Treasure Cruise',
+            'Dig Through Time',
+            'Ancient Den',
+            'Blazing Shoal',
+            'Chrome Mox',
+            'Cloudpost',
+            'Dark Depths',
+            'Deathrite Shaman',
+            'Dread Return',
+            'Eye of Ugin',
+            'Glimpse of Nature',
+            'Golgari Grave-Troll',
+            'Great Furnace',
+            'Green Sun\'s Zenith',
+            'Hypergenesis',
+            'Mental Misstep',
+            'Ponder',
+            'Preordain',
+            'Punishing Fire',
+            'Rite of Flame',
+            'Seat of the Synod',
+            'Second Sunrise',
+            'Seething Song',
+            'Sensei\'s Divining Top',
+            'Skullclamp',
+            'Splinter Twin',
+            'Summer Bloom',
+            'Tree of Tales',
+            'Umezawa\'s Jitte',
+            'Vault of Whispers',
+        ]
+    def getNextCard(self):
+        self.ITERATOR += 1
+        index = ITERATOR % len(self.cards)
+        return cards[index]
+
+
+
+def nicknames(name):
+    try:
+        nicks = {"bob": "Dark Confidant",
+                 "gary": "Gray Merchant of Asphodel",
+                 "sad robot": "Solemn Simulacrum",
+                 "jens": "Solemn Simulacrum",
+                 "bolt": "Lightning Bolt",
+                 "path": "Path to Exile",
+                 "snappy": "Snapcaster Mage",
+                 "tiago chan": "Snapcaster Mage",
+                 "goyf": "Tarmogoyf",
+                 "taylor swift": "Monastery Swiftspear",
+                 "mom": "Mother of Runes",
+                 "bfm": "B.F.M. (Big Furry Monster)",
+                 "i can't even": "Void Winnower",
+                 "durdle turtle": "Meandering Towershell",
+                 "tim": "Prodigal Sorcerer",
+                 "ernie": "Ernham Djinn",
+                 "wog": "Wrath of God",
+                 "finkel": "Shadowmage Infiltrator",
+                 "jon finkel": "Shadowmage Infiltrator",
+                 "titi": "Thing in the Ice",
+                 "chris pikula": "Meddling Mage",
+                 "superman": "Morphling",
+                 "gitgud frog": "The Gitrog Monster",
+                 "poyser": "Doomed Traveler",
+                 "bourne": "Look at me I'm the DCI",
+                 "ged": "Grimgrin",
+                 "simoon": "Omniscience",
+                 "lyndon": "Rakdos, Lord",
+                 "ben": "Narset, Enlightened",
+                 "josh": "Sowing Salt",
+                 "chris": "capsize",
+                 "robinson": "Derevi",
+                 "merrison": "General Tazri",
+                 "science": "Enter the Infinite",
+                 "skittles": "Skithiryx",
+                 "baby jace": "Jace, Vryn",
+                 "lotv": "Liliana of the Veil",
+                 "jarvis": "Reclusive Artificer"
+                 }
+        return nicks[name.lower()]
+    except KeyError:
+        return name
+
+
+def properNames(corseMatch, name):
+    # Can probably improve this so it returns the proper capitalisations
+    # for now standardise as lowercase
+    corseMatchLower = [l.lower() for l in corseMatch]
+    if name in corseMatchLower:
+        # if there is an exact full match, return it
+        return [name]
+    # Prioritise cardnames which contain the search-word surrounded by word boundries
+    wordBoundaryPriorityRegex = re.compile(".*\\b{}\\b.*".format(name))
+    boundryMatch = [m.group(0) for l in corseMatchLower for m in [wordBoundaryPriorityRegex.search(l)] if m]
+    if boundryMatch:
+        corseMatchLower = boundryMatch #filter results which did not pass the word boundary filter
+    NamePriorityRegex = re.compile("^{}(([,])|( of )|( the )).*".format(name.lower()))
+    shortnames = [m.group(0) for l in corseMatchLower for m in [NamePriorityRegex.search(l)] if m]
+    if shortnames:
+        return shortnames
+    else:
+        return corseMatchLower
+
+
+def sequenceMatch(closeMatch, name):
+    score = 0
+    best = ""
+    for i in closeMatch:
+        newscore = SequenceMatcher(None, name.lower(), i.lower()).ratio()
+        if newscore > score:
+            best = i
+            score = newscore
+    return best
